c560e8485788fd4ae418a13a37f133c28c6d0583
sungmin <sungmin69355@naver.com>
2020-09-11 22:59:56 +0900

    디버그 설정

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fqjng2@xhvxk&knwqjdx8jwo)e*2w(d&@quubcl-nq-q-jofjk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
   '*'
